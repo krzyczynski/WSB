4c3cb158fd2fa4f0ab2775c76220a6c8a48b34d4
Robert Krzyczyński <rkrzyczynski@gmail.com>
2019-10-05 22:14:05 +0200

    Zadanie_1 problem solved

---
+++ M.Szyper/Wprowadzenie/Zadanie_1/Zadanie_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program oblicza pole prostokąta.");
+            Console.WriteLine("");
+
+            int test0=0, test1=0;
+            float a=0, b=0;
+
+            Console.Write("Podaj bok a: ");
+            string str = Console.ReadLine();
+
+            if (float.TryParse(str, out a) == false)
+                Console.WriteLine("zły format liczby");
+            else
+                test0=1;
+            
+            if (test0 == 1)
+            {
+                Console.Write("Podaj bok b: ");
+                str = Console.ReadLine();
+
+                    if (float.TryParse(str, out b) == false)
+                    Console.WriteLine("zły format liczby");
+                else
+                    test1 = 1;
+            }
+
+
+            if ( (test0 == 1)&&(test1 == 1))
+               Console.WriteLine("Pole prostokąta wynosi: " + a * b + " cm/kw");
+           
+        }
+    }
+}
