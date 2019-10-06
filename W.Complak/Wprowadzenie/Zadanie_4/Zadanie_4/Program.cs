76ff9fb6a834383aa0a0cbf94c178fc53cf2abde
Robert Krzyczyński <rkrzyczynski@gmail.com>
2019-10-06 21:04:20 +0200

    zadanie4 still not working

--- W.Complak/Wprowadzenie/Zadanie_4/Zadanie_4/Program.cs
+++ W.Complak/Wprowadzenie/Zadanie_4/Zadanie_4/Program.cs
@@ -7,24 +7,36 @@ namespace Zadanie_4
         static void Main(string[] args)
         {
             /*Napisać program obliczania sumy cyfr rozwinięcia dziesiętnego dla zadanej liczby naturalnej.*/
+
+            int sum = 0;
+            int number = 0;
+            char x;
+           
+
+
             Console.WriteLine("Program do zadania 4");
             Console.WriteLine("Program oblicza sumę cyfr rozwinięcia dziesiętnego podanej liczby.");
             Console.WriteLine("");
 
             Console.Write("Podaj liczbe: ");
-    
 
-            int sum = 0;
-        
+            string numberIn = Console.ReadLine();
 
-            for (int i = 0; i < 10; i++)
+
+            for (int i = 0; i < 5; i++)
             {
-                
+                //Console.WriteLine(numberIn[i]);
+                x = numberIn[i];
+                number = int.Parse(x) ;
+                sum += number;
+
             }
 
-            Console.WriteLine("Suma cyfr rozwinięcia dziesiętnego wynosi:{0}", sum);
+            Console.WriteLine("Suma cyfr rozwinięcia dziesiętnego wynosi: {0}", sum);
 
-            //NIE DZIAŁA!!!
+            //NIE DZIAŁA ;-;
+
+            
 
         }
     }
