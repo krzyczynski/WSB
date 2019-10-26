f382d55a542e67a6ec3bf57916581f314fd62859
Robert Krzyczyński <rkrzyczynski@gmail.com>
2019-10-26 19:30:45 +0200

    task solved

---
+++ M.Szyper/Zajęcia/Tablice/Tablice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfColours;
+            
+            Console.WriteLine("Program z zajęć.\n");
+            Console.Write("Podaj wielkść tablicy: ");
+            for (; ; )
+            {
+                if (int.TryParse(Console.ReadLine(), out numberOfColours) == false) Console.WriteLine("Błąd danych wejścia");
+                else break;
+            }
+
+            string[] arrayColours = new string[numberOfColours];
+
+            for (int i = 0; i < numberOfColours; i++)
+            {
+                Console.Write("Podaj {0} kolor: ", i+1);
+                arrayColours[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < numberOfColours; i++)
+            {
+                Console.WriteLine("Twój {0} ulubiony: {1}", i + 1, arrayColours[i]);
+            }
+
+        }
+    }
+}
---
+++ M.Szyper/Zajęcia/Tablice_wielowymiarowe/Tablice_wielowymiarowe/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tablice_wielowymiarowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadanie dodatkowe z użyciem pętli Foreach
+            //Wykonał Robert Krzyczyński
+
+            const int arrayX = 3;
+            const int arrayY = 3;
+
+        
+
+            int[,] array = new int[arrayX, arrayY];
+
+         
+
+            for (int i = 0; i<array.GetLength(0); i++) //wypełnianie tablicy
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = i * 3 + j;
+                }
+            }
+
+            int k = 0;
+            foreach(int value in array) //Wypisywanie foreach
+            { 
+                if (k != 0 && k % array.GetLength(1) == 0) Console.WriteLine();
+                Console.Write(value + "\t");
+                k++;
+            }
+        }
+    }
+}
